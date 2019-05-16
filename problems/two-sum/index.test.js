7b65755cee01a53eb8d7e99ddfecd6568d5eaf10
voodee <eedoov@gmail.com>
2019-05-17 01:03:21 +0300

    add descriptio in problems - Add Sum

--- problems/two-sum/index.test.js
+++ problems/two-sum/index.test.js
@@ -1,7 +1,7 @@
-const twoSum = require("./");
+const twoSumMedium = require("./medium");
 
 describe("Two Sum", () => {
   it("if put [2,7,11,15], 9 then return [0,1]", () => {
-    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
+    expect(twoSumMedium([2, 7, 11, 15], 9)).toEqual([0, 1]);
   });
 });
